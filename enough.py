ff87fb9f1d5efcda18ea5a848082ae04f23d7582
turkmeninho <turkmenalimirac@gmail.com>
2026-01-06 16:26:13 +0300

    enough.py

--- enough.py
+++ enough.py
@@ -15,16 +15,15 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
-     ______                         _     
-    |  ____|                       | |    
-    | |__   _ __   ___  _   _  __ _| |__  
-    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
-    | |____| | | | (_) | |_| | (_| | | | |
-    |______|_| |_|\___/ \__,_|\__, |_| |_|
-                               __/ |      
-                              |___/      
+ ██████╗ ██╗ ██████╗ ███╗   ███╗
+ ██╔══██╗██║██╔════╝ ████╗ ████║
+ ██████╔╝██║██║  ███╗██╔████╔██║
+ ██╔══██╗██║██║   ██║██║╚██╔╝██║
+ ██████╔╝██║╚██████╔╝██║ ╚═╝ ██║
+ ╚═════╝ ╚═╝ ╚═════╝ ╚═╝     ╚═╝
+          iOS Edition   
     
-    Sms: {}           {}by {}@tingirifistik\n  
+    Sms: {}           {}by {}@bigm\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
